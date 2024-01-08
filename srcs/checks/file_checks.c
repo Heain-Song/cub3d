88bbf5c0fae49589978832becf9864abdc9b4e22
Emmanuel de Sigalony <ede-siga@localhost.localdomain>
2024-01-08 16:29:07 +0100

    Changed file error messaged to be like the subject

--- srcs/checks/file_checks.c
+++ srcs/checks/file_checks.c
@@ -6,10 +6,16 @@
 /*   By: ede-siga <ede-siga@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 16:02:06 by ede-siga          #+#    #+#             */
-/*   Updated: 2024/01/08 16:17:52 by ede-siga         ###   ########.fr       */
+/*   Updated: 2024/01/08 16:28:05 by ede-siga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <cub3d.h>
+int	file_error(char *str)
+{
+	ft_putstrfd("Error\n", 2);
+	ft_putstrfd(str, 2);
+	return (-1);
+}
 
 int	check_file_name(char *file_name)
 {
@@ -28,16 +34,13 @@ int	check_open(char *file)
 
 	fd = open(file, O_RDONLY);
 	if (fd <= 0)
-		ft_putstrfd("Can't open file\n", 2);
+		return (file_error("Can't open file\n"));
 	return (fd);
 }
 
 int	file_checker(char *file)
 {
 	if (check_file_name(file))
-	{
-		ft_putstrfd("Wrong file extension\n", 2);
-		return (-1);
-	}
+		return (file_error("Wrong file extension\n"));
 	return (check_open(file));
 }
