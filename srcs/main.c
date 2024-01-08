29d9bb3df7400cd22cf7624f2bdf63a430922fbd
Emmanuel de Sigalony <ede-siga@localhost.localdomain>
2024-01-08 15:05:27 +0100

    Normalized

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: hesong <hesong@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 17:35:13 by hesong            #+#    #+#             */
-/*   Updated: 2024/01/08 14:51:37 by ede-siga         ###   ########.fr       */
+/*   Updated: 2024/01/08 15:04:39 by ede-siga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,21 +23,20 @@ int	check_file_name(char *file_name)
 	return (0);
 }
 
-
 /*int	validate_map(char **argv)
 {
 	if (check_file_name(argv[1]) != 0)
 
 	}*/
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
 	if (argc == 2)
 	{
 		if (!check_file_name(argv[1]))
 			printf("SUCCESS\n");
 		else
-			printf("Wrong file name\n");
+			printf("Wrong file xname\n");
 	}
 	else
 		printf("Argc Error. \n");
