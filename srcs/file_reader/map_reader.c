c467adcc9f771f39b745b19f84d0e3d6101b3d63
Emmanuel de Sigalony <ede-siga@localhost.localdomain>
2024-01-16 18:16:00 +0100

    normalized map reader

--- srcs/file_reader/map_reader.c
+++ srcs/file_reader/map_reader.c
@@ -6,12 +6,9 @@
 /*   By: ede-siga <ede-siga@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/12 15:07:21 by ede-siga          #+#    #+#             */
-/*   Updated: 2024/01/15 10:49:58 by ede-siga         ###   ########.fr       */
+/*   Updated: 2024/01/16 18:15:40 by ede-siga         ###   ########.fr       */
 /*                                                                            */
-/* **********************************************************************
-**** */
-
-
+/* ************************************************************************** */
 #include <cub3d.h>
 
 t_elems	error_reading(char *temp, char *error, t_elems elems)
@@ -22,7 +19,23 @@ t_elems	error_reading(char *temp, char *error, t_elems elems)
 	ft_putstrfd(error, 2);
 	elems.error = 1;
 	return (elems);
-	
+}
+
+char	*skip_to_map(int fd)
+{
+	char	*temp;
+	int		read;
+
+	temp = NULL;
+	read = 0;
+	if (read == 0)
+		return (NULL);
+	while (!ft_strcmp(temp, "\n"))
+	{
+		free(temp);
+		temp = basic_gnl(fd, &read, 1);
+	}
+	return (temp);
 }
 
 t_elems	start_reading_map(int fd, t_elems elems)
@@ -30,16 +43,9 @@ t_elems	start_reading_map(int fd, t_elems elems)
 	char	*temp;
 	int		read;
 
-	temp = NULL;
-	read = 0;
-	temp = basic_gnl(fd, &read, 1);
-	if (read == 0)
+	temp = skip_to_map(fd);
+	if (temp == NULL)
 		return (error_reading(temp, "Error\n map not found\n", elems));
-	while (!ft_strcmp(temp, "\n"))
-	{
-		free(temp);
-		temp = basic_gnl(fd, &read, 1);
-	}
 	elems.map = add_to_table(temp, elems.map);
 	free(temp);
 	read = 1;
@@ -58,6 +64,4 @@ t_elems	start_reading_map(int fd, t_elems elems)
 		}
 	}
 	return (elems);
-//read one line;
-	
 }
