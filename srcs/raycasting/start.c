ead318a62a9b6daa1ff3c7625c5d8f3bc928f980
Hea in SONG <heain.song328@gmail.com>
2024-02-21 16:33:24 +0100

    added forgotten file in change

--- srcs/raycasting/start.c
+++ srcs/raycasting/start.c
@@ -6,13 +6,13 @@
 /*   By: hesong <hesong@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 20:55:16 by hesong            #+#    #+#             */
-/*   Updated: 2024/02/21 15:21:06 by hesong           ###   ########.fr       */
+/*   Updated: 2024/02/21 15:46:43 by hesong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-void	init_texture_to_buffer(t_elems *elems)
+/*void	init_texture_to_buffer(t_elems *elems)
 {
 	unsigned int	i;
 	int				x;
@@ -27,7 +27,7 @@ void	init_texture_to_buffer(t_elems *elems)
 		i++;
 		textures = textures->next;
 	}
-}
+}*/
 
 void	get_plane(t_elems *elems)
 {
@@ -62,11 +62,11 @@ void	init_before_raycasting(t_elems *elems)
 	elems->ray.pos_x = elems->player_x + 0.5; // x start position
 	elems->ray.pos_y = elems->player_y + 0.5; // y start position
 	get_dir(elems);
-	//elems->dirX = -1; // initial direction vector x
+	//elems->ray.dir_x = -1; // initial direction vector x
 	//elems->dirY = 0; // initial direction vector y
 	get_plane(elems);
-	//elems->planeX = 0; //the 2d raycaster version of camera plane x
-	//elems->planeY = 0.66; //the 2d raycaster version of camera plane y
+	//elems->ray.plane_x = 0; //the 2d raycaster version of camera plane x
+	//elems->ray.plane_y = 0.66; //the 2d raycaster version of camera plane y
 	elems->ray.time = 0; //time of current frame
 	elems->ray.old_time = 0; //time of previous frame
 	elems->ray.move_speed = 0.05;
@@ -108,13 +108,6 @@ t_elems	start_raycasting(t_elems elems)
 		elems.error = 1;
 		return (elems);
 	}
-	elems.texture_int_array = calloc(5, sizeof(int *)); //ft_calloc
-	if (!elems.texture_int_array)
-	{
-		elems.error = 1;
-		return (elems);
-	}
-
 	//mlx_hook(elems.mlx.window, 2, 1L << 0, ft_close_win, &elems.mlx);
 	mlx_hook(elems.mlx.window, 2, 1L << 0, pressed_key, &elems.mlx);
 	//mlx_hook(elems.mlx.window, 3, 1L << 1, key_release, &elems);
