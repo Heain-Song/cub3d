3a5bb3f1827c94ff165e9ba3b02a0c8fe7c6daa9
Emmanuel de Sigalony <ede-siga@localhost.localdomain>
2024-02-27 19:33:57 +0100

    fixed player starting position

--- srcs/raycasting/start.c
+++ srcs/raycasting/start.c
@@ -6,7 +6,7 @@
 /*   By: hesong <hesong@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 20:55:16 by hesong            #+#    #+#             */
-/*   Updated: 2024/02/27 19:09:13 by ede-siga         ###   ########.fr       */
+/*   Updated: 2024/02/27 19:29:55 by ede-siga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,8 @@ void	get_position(t_elems *elems)
 {
 	elems->player_x += 0.5; //just to fix camera at start to be in center
 	elems->ray.pos_x = elems->player_x;// x start position
-	elems->ray.pos_y = elems->player_y; // y start position
+	//elems->player_x += 0.5;
+	elems->ray.pos_y = elems->player_y + 1; // y start position;
 }
 
 t_elems	launch_game(t_elems elems)
