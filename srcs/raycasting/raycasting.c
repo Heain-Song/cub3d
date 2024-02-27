37150ea1901f48bfd96dcaa863229e3f3e332cd6
Emmanuel de Sigalony <ede-siga@localhost.localdomain>
2024-02-28 00:41:14 +0100

    fixed segfault when exiting game

--- srcs/raycasting/raycasting.c
+++ srcs/raycasting/raycasting.c
@@ -6,7 +6,7 @@
 /*   By: hesong <hesong@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 15:49:29 by hesong            #+#    #+#             */
-/*   Updated: 2024/02/27 17:32:35 by hesong           ###   ########.fr       */
+/*   Updated: 2024/02/28 00:40:32 by ede-siga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -74,11 +74,11 @@ void	get_sidedist(t_elems *elems)
 void	get_deltadist(t_elems *elems)
 {
 	if (elems->ray.raydir_x == 0)
-		elems->ray.deltadist_x = 9999999;
+		elems->ray.deltadist_x = 0;
 	else
 		elems->ray.deltadist_x = fabs(1 / elems->ray.raydir_x);
 	if (elems->ray.raydir_y == 0)
-		elems->ray.deltadist_y = 9999999;
+		elems->ray.deltadist_y = 0;
 	else
 		elems->ray.deltadist_y = fabs(1 / elems->ray.raydir_y);
 }
@@ -86,7 +86,7 @@ void	get_deltadist(t_elems *elems)
 void	get_camerax(t_elems *elems, int x)
 {
 	elems->ray.camera_x = 2 * x / (double)WIDTH - 1;
-	elems->ray.raydir_x= elems->ray.dir_x + elems->ray.plane_x * elems->ray.camera_x;
+	elems->ray.raydir_x = elems->ray.dir_x + elems->ray.plane_x * elems->ray.camera_x;
 	elems->ray.raydir_y = elems->ray.dir_y + elems->ray.plane_y * elems->ray.camera_x;
 }
 
@@ -124,6 +124,8 @@ void	raycast(t_elems *elems)
 
 int	main_loop(t_elems *elems)
 {
+	if (!elems->mlx.server || !elems->mlx.window)
+		return (-1);
 	elems->screen.img = mlx_new_image(elems->mlx.server, WIDTH, HEIGHT);
 	elems->screen.addr = mlx_get_data_addr(elems->screen.img,
 			&elems->screen.bits_per_pixel,
