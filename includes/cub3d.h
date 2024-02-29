9113a00d6ca4256b70039c444a07ccacf193bbb5
Emmanuel de Sigalony <ede-siga@localhost.localdomain>
2024-02-29 22:26:42 +0100

    header normalised

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: hesong <hesong@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/16 07:53:34 by hesong            #+#    #+#             */
-/*   Updated: 2024/02/29 22:05:48 by ede-siga         ###   ########.fr       */
+/*   Updated: 2024/02/29 22:26:25 by ede-siga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,17 +43,20 @@
 
 /***************STRUCTURES***************/
 
-typedef struct s_vector{
+typedef struct s_vector
+{
 	double	x;
 	double	y;
 }	t_vector;
 
-typedef struct s_player{
+typedef struct s_player
+{
 	int		x;
 	int		y;
 }	t_player;
 
-typedef struct	s_data {
+typedef struct s_data
+{
 	void	*img;
 	char	*addr;
 	int		bits_per_pixel;
@@ -61,7 +64,7 @@ typedef struct	s_data {
 	int		endian;
 }	t_data;
 
-typedef struct	s_num_mlx
+typedef struct s_num_mlx
 {
 	void	*server;
 	void	*window;
@@ -145,100 +148,92 @@ typedef struct s_elems
 	bool		rotate_right;
 	void		*screenbuffer;
 	char		*map_file;
-
-
-
-
-
 }	t_elems;
 
 /****************FUNCTIONS***************/
 
-int			ft_strcmp(const char *s1, const char *s2);
-void		ft_putstrfd(char *str, int fd);
-int			file_checker(char *str);
-char		*basic_gnl(int fd, int *read_amount, int keep_nl);
-int			ft_strlen(char *str);
-char		*ft_strcpy(char *src, char *dest);
-t_textures	*make_text_id(void);
-void		free_t_textures(t_textures *textures, t_elems elems);
-char		*ft_strdup(char *str);
-int			ft_strncmp(const char *s1, const char *s2, unsigned int n);
-t_elems		element_reader(int fd, t_elems elems);
-t_elems		init_elems(void);
-t_elems		check_elems(t_elems elems_list);
-int			elem_type(char *temp, char **str);
-int			ft_atoi(const char *str);
-t_elems		get_nbr_atribs(t_elems elems, char *str);
-t_elems		get_str_atrib(t_elems elems, char *str);
-t_elems		assign_which_elem(char *str, int nb, t_elems elems);
-void		free_elems(t_elems elems);
-char		*skip_and_getnb(char *str, int *nb);
-int			str_check_num_amount(char *str);
-t_elems		start_reading_map(int fd, t_elems elems);
-char		**add_to_table(char *str, char **table);
-t_elems		check_map(t_elems elems, char **map);
-t_elems		error_node(t_textures *node, t_elems elems);
-t_elems		basic_error(t_elems elems, char *error,
-				char *attrib, char *to_free);
-t_elems		save_error(t_elems elems);
-t_elems		check_around(char d, t_elems elems);
-int			is_player(char c);
-t_elems		find_player(char **map, t_elems elems);
-t_elems		error_reading(char *temp, char *error, t_elems elems);
-t_elems		check_textures(t_elems elems, t_textures *node);
-t_elems		check_empty_line(char **map, size_t i, t_elems elems);
-t_elems		error_player(t_elems elems);
-t_elems		check_type_in_map(char **str, t_elems elems);
-t_elems		color_checker(t_elems elems, char *str);
-t_elems		start_window_loop(t_elems elems);
-int			get_colors(int t, int r, int g, int b);
-void		draw_iteration(t_elems *elems, int x);
-void		get_lineinfo(t_elems *elems);
-void		get_f_c_colors(int *f_colors, int *c_colors, t_elems *elems);
+int				ft_strcmp(const char *s1, const char *s2);
+void			ft_putstrfd(char *str, int fd);
+int				file_checker(char *str);
+char			*basic_gnl(int fd, int *read_amount, int keep_nl);
+int				ft_strlen(char *str);
+char			*ft_strcpy(char *src, char *dest);
+t_textures		*make_text_id(void);
+void			free_t_textures(t_textures *textures, t_elems elems);
+char			*ft_strdup(char *str);
+int				ft_strncmp(const char *s1, const char *s2, unsigned int n);
+t_elems			element_reader(int fd, t_elems elems);
+t_elems			init_elems(void);
+t_elems			check_elems(t_elems elems_list);
+int				elem_type(char *temp, char **str);
+int				ft_atoi(const char *str);
+t_elems			get_nbr_atribs(t_elems elems, char *str);
+t_elems			get_str_atrib(t_elems elems, char *str);
+t_elems			assign_which_elem(char *str, int nb, t_elems elems);
+void			free_elems(t_elems elems);
+char			*skip_and_getnb(char *str, int *nb);
+int				str_check_num_amount(char *str);
+t_elems			start_reading_map(int fd, t_elems elems);
+char			**add_to_table(char *str, char **table);
+t_elems			check_map(t_elems elems, char **map);
+t_elems			error_node(t_textures *node, t_elems elems);
+t_elems			basic_error(t_elems elems, char *error,
+					char *attrib, char *to_free);
+t_elems			save_error(t_elems elems);
+t_elems			check_around(char d, t_elems elems);
+int				is_player(char c);
+t_elems			find_player(char **map, t_elems elems);
+t_elems			error_reading(char *temp, char *error, t_elems elems);
+t_elems			check_textures(t_elems elems, t_textures *node);
+t_elems			check_empty_line(char **map, size_t i, t_elems elems);
+t_elems			error_player(t_elems elems);
+t_elems			check_type_in_map(char **str, t_elems elems);
+t_elems			color_checker(t_elems elems, char *str);
+t_elems			start_window_loop(t_elems elems);
+int				get_colors(int t, int r, int g, int b);
+void			draw_iteration(t_elems *elems, int x);
+void			get_lineinfo(t_elems *elems);
+void			get_f_c_colors(int *f_colors, int *c_colors, t_elems *elems);
 
 //visuals
-int			window_destroyer(t_num_mlx *mlx);
-int			ft_close_win(int keycode, t_num_mlx *mlx);
-//int			hook_event(t_num_mlx *mlx);
+int				window_destroyer(t_num_mlx *mlx);
+int				ft_close_win(int keycode, t_num_mlx *mlx);
 
 //raycasting//
-t_elems		launch_game(t_elems elems);
-void		get_position(t_elems *elems);
-void		get_dir(t_elems *elems);
-void		get_plane(t_elems *elems);
-
-
+t_elems			launch_game(t_elems elems);
+void			get_position(t_elems *elems);
+void			get_dir(t_elems *elems);
+void			get_plane(t_elems *elems);
 
 //debug//
-void		display_textures_list(t_textures *head);
-void		print_map(char **map);
-void		loop_debug(t_elems elems);
-t_elems		init_mlx(t_elems elems);
-void		clean_mlx(t_num_mlx mlx_info);
-void		print_elems_map(t_elems *elems);
+void			display_textures_list(t_textures *head);
+void			print_map(char **map);
+void			loop_debug(t_elems elems);
+t_elems			init_mlx(t_elems elems);
+void			clean_mlx(t_num_mlx mlx_info);
+void			print_elems_map(t_elems *elems);
 
 //raycasting
 int				main_loop(t_elems *elems);
 void			draw_line(t_elems *elems, int x);
 unsigned int	get_pixel_color(t_data *data, int x, int y);
 void			put_pixel(t_data *data, int y, int x, int color);
-char	*ft_strchr(const char *s, int c);
-int	save_texture(t_elems *elems);
-bool	load_image(t_elems *elems, t_textures *current, int index);
-void	get_dist(t_elems *elems);
+char			*ft_strchr(const char *s, int c);
+int				save_texture(t_elems *elems);
+bool			load_image(t_elems *elems, t_textures *current, int index);
+void			get_dist(t_elems *elems);
 
 //key_hooks
 
-int 	pressed_key(int keycode, t_elems *elems);
-void 	rot_plus(t_elems *elems);
-void 	rot_minus(t_elems *elems);
-void	move_minus(t_elems *elems);
-void	move_plus(t_elems *elems);
-void	move_right(t_elems *elems);
-void	move_left(t_elems *elems);
-int		wall_check_move_up(t_elems *elems);
-void	center_cam(t_elems *elems);
-int		wall_check_move_left(t_elems *elems);
-int		wall_check_move_right(t_elems *elems);
+int				pressed_key(int keycode, t_elems *elems);
+void			rot_plus(t_elems *elems);
+void			rot_minus(t_elems *elems);
+void			move_minus(t_elems *elems);
+void			move_plus(t_elems *elems);
+void			move_right(t_elems *elems);
+void			move_left(t_elems *elems);
+int				wall_check_move_up(t_elems *elems);
+void			center_cam(t_elems *elems);
+int				wall_check_move_left(t_elems *elems);
+int				wall_check_move_right(t_elems *elems);
 #endif
