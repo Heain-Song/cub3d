725d30ae1f109704257e40ead5f9e7f0b8fd84f8
Emmanuel de Sigalony <ede-siga@localhost.localdomain>
2024-02-29 21:32:04 +0100

    normalised mlx_init

--- srcs/mlx_manage/mlx_init.c
+++ srcs/mlx_manage/mlx_init.c
@@ -6,7 +6,7 @@
 /*   By: hesong <hesong@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 10:19:42 by ede-siga          #+#    #+#             */
-/*   Updated: 2024/02/29 12:12:26 by hesong           ###   ########.fr       */
+/*   Updated: 2024/02/29 21:31:43 by ede-siga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,15 +21,4 @@ t_elems	init_mlx(t_elems elems)
 	elems.mlx.window = mlx_new_window(elems.mlx.server, WIDTH, HEIGHT, PROG);
 	if (!elems.mlx.window)
 		return (error_reading(NULL, "mlx window failed to create.\n", elems));
-	/*elems.img.img = mlx_new_image(elems.mlx.server, WIDTH, HEIGHT);
-	if (!elems.img.img)
-		return (error_reading(NULL, "mlx img failed to create.\n", elems));
-	elems.img.addr = mlx_get_data_addr(elems.img.img,
-			&elems.img.bits_per_pixel,
-			&elems.img.line_length,
-			&elems.img.endian);
-	if (!elems.img.addr)
-		return (error_reading(NULL, "mlx img addr failed to create.\n", elems));
-		mlx_put_image_to_window(elems.mlx.server, elems.mlx.window, elems.img.img, 0, 0);*/
-	return (elems);
 }
